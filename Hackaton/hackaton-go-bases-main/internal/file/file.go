62d4dd04732e01cb293cb94eb076262f722d21da
MarianoLibre <mariano.macri@mercadolibre.com>
2022-09-29 21:44:03 -0300

    hackathon: add ticket to file

--- Hackaton/hackaton-go-bases-main/internal/file/file.go
+++ Hackaton/hackaton-go-bases-main/internal/file/file.go
@@ -1,9 +1,10 @@
 package file
 
 import (
+	"fmt"
 	"os"
+	"strconv"
 	"strings"
-    "strconv"
 
 	"github.com/bootcamp-go/hackaton-go-bases/internal/service"
 )
@@ -39,11 +40,28 @@ func (f *File) Read() ([]service.Ticket, error) {
         }
         tickets = append(tickets, newTicket)
     }
-    
 	return tickets, nil
 }
 
-func (f *File) Write(service.Ticket) error {
+func (f *File) Write(s *service.Ticket) error {
+    newTicket := fmt.Sprintf(
+        "\n%d,%s,%s,%s,%s,%d", 
+        s.Id,
+        s.Names,
+        s.Email,
+        s.Destination,
+        s.Date,
+        s.Price,
+    )
+    destination, err := os.OpenFile( f.Path, os.O_WRONLY | os.O_APPEND, 0666 )
+    if err != nil {
+        return err
+    }
+    _, err2 := destination.Write([]byte( newTicket ))
+    if err2 != nil {
+        fmt.Println(err)
+    }
+    destination.Close()
 	return nil
 }
 
--- Hackaton/hackaton-go-bases-main/main.go
+++ Hackaton/hackaton-go-bases-main/main.go
@@ -14,6 +14,21 @@ func main() {
         panic(err)
     }
     fmt.Println(data[0])
+
+    t := service.Ticket{
+        Id: 666,
+        Names: "Judas Priest",
+        Email: "judas@priest.com",
+        Destination: "London",
+        Date: "11:11",
+        Price: 1_000,
+    }
+
+    err2 := f.Write(&t)
+    if err2 != nil {
+        fmt.Println(err2)
+    }
+
 	var tickets []service.Ticket
 	// Funcion para obtener tickets del archivo csv
 	service.NewBookings(tickets)
