50bc91e576042252d8c4e40bb04a3d87700204c5
MarianoLibre <mariano.macri@mercadolibre.com>
2022-09-29 21:05:16 -0300

    hackathon: implement reader module

--- Hackaton/hackaton-go-bases-main/internal/file/file.go
+++ Hackaton/hackaton-go-bases-main/internal/file/file.go
@@ -1,15 +1,49 @@
 package file
 
-import "github.com/bootcamp-go/hackaton-go-bases/internal/service"
+import (
+	"os"
+	"strings"
+    "strconv"
+
+	"github.com/bootcamp-go/hackaton-go-bases/internal/service"
+)
 
 type File struct {
-	path string
+	Path string
 }
 
 func (f *File) Read() ([]service.Ticket, error) {
-	return nil, nil
+    data, err := os.ReadFile(f.Path)
+    if err != nil {
+        return nil, err
+    }
+    content := strings.Split(string(data), "\n")
+    tickets := []service.Ticket{}
+    for _, line := range content {
+        fields := strings.Split(line, ",")
+        id, err := strconv.Atoi(fields[0])
+        if err != nil {
+            return nil, err
+        }
+        price, err := strconv.Atoi(fields[5])
+        if err != nil {
+            return nil, err
+        }
+        newTicket := service.Ticket{
+            Id: id,
+            Names: fields[1],
+            Email: fields[2],
+            Destination: fields[3],
+            Date: fields[4],
+            Price: price,
+        }
+        tickets = append(tickets, newTicket)
+    }
+    
+	return tickets, nil
 }
 
 func (f *File) Write(service.Ticket) error {
 	return nil
 }
+
--- Hackaton/hackaton-go-bases-main/main.go
+++ Hackaton/hackaton-go-bases-main/main.go
@@ -1,8 +1,19 @@
 package main
 
-import "github.com/bootcamp-go/hackaton-go-bases/internal/service"
+import (
+	"fmt"
+
+	"github.com/bootcamp-go/hackaton-go-bases/internal/service"
+    "github.com/bootcamp-go/hackaton-go-bases/internal/file"
+)
 
 func main() {
+    f := file.File{Path: "/Users/MMACRI/Bootcamp/backpack-bcgow6-mariano-macri/Hackaton/hackaton-go-bases-main/tickets.csv"} 
+    data, err := f.Read()
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println(data[0])
 	var tickets []service.Ticket
 	// Funcion para obtener tickets del archivo csv
 	service.NewBookings(tickets)
