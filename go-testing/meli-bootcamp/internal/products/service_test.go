74c2424032a66d4be70f86c539ded1f4a9339be5
MarianoLibre <mariano.macri@mercadolibre.com>
2022-10-20 21:28:21 -0300

    C2-Testing: Ejercicio 1 - Service/Repo/Db Update()

--- go-testing/meli-bootcamp/internal/products/service_test.go
+++ go-testing/meli-bootcamp/internal/products/service_test.go
@@ -1,6 +1,11 @@
 package products
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 type MockStorage struct {
 	dataMock       []Product
@@ -28,4 +33,49 @@ func (m *MockStorage) Write(data interface{}) error {
 	a := data.([]Product)
 	m.dataMock = append(m.dataMock, a[len(a)-1])
 	return nil
+}
+
+func TestServiceUpdate(t *testing.T) {
+	product := Product{
+		Id:        1,
+		Name:      "tablet",
+		Colour:    "black",
+		Price:     200.5,
+		Stock:     12,
+		Code:      "XXX",
+		Published: true,
+		CreatedAt: "today",
+	}
+	updated := Product{
+		Id:        1,
+		Name:      "cellphone",
+		Colour:    "red",
+		Price:     500.5,
+		Stock:     10,
+		Code:      "XXX",
+		Published: true,
+		CreatedAt: "today",
+	}
+	database := []Product{
+		product,
+	}
+	mockedStorage := MockStorage{
+		dataMock: database,
+	}
+	rp := NewRepository(&mockedStorage)
+	svc := NewService(rp)
+
+	result, err := svc.Update(
+		updated.Id, 
+		updated.Name, 
+		updated.Colour, 
+		updated.Code, 
+		updated.CreatedAt, 
+		updated.Stock, 
+		updated.Price, 
+		updated.Published)
+
+	assert.Nil(t, err)
+	assert.Equal(t, result, updated)
+	assert.True(t, mockedStorage.readWasCalled)
 }
