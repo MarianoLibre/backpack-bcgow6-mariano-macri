4723645de6a7979cbf55c0605670baa95a8e88e5
MarianoLibre <mariano.macri@mercadolibre.com>
2022-10-21 12:50:33 -0300

    Add err to Read/Write and apply gofmt

--- go-testing/meli-bootcamp/internal/products/repository_test.go
+++ go-testing/meli-bootcamp/internal/products/repository_test.go
@@ -7,15 +7,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-
-
-type StubStore struct{
-	mockedData []Product
-	readWasCalled bool
+type StubStore struct {
+	mockedData     []Product
+	readWasCalled  bool
 	writeWasCalled bool
+	errRead        string
+	errWrite       string
 }
 
 func (s *StubStore) Read(data interface{}) error {
+	if s.errRead != "" {
+		return errors.New("error: can't read data")
+	}
 	s.readWasCalled = true
 	a, ok := data.(*[]Product)
 	if !ok {
@@ -25,31 +28,34 @@ func (s *StubStore) Read(data interface{}) error {
 	return nil
 }
 
-func (s *StubStore) Write(data interface{}) error{
+func (s *StubStore) Write(data interface{}) error {
+	if s.errWrite != "" {
+		return errors.New("error: can't write data")
+	}
 	s.writeWasCalled = true
 	return nil
 }
 
-
+// GetAll
 func TestGetAll(t *testing.T) {
 	expected := []Product{
 		{
-			Id: 1,
-			Name: "Before updated",
-			Colour: "black",
-			Price: 200.5,
-			Stock: 12,
-			Code: "XXX",
+			Id:        1,
+			Name:      "Before updated",
+			Colour:    "black",
+			Price:     200.5,
+			Stock:     12,
+			Code:      "XXX",
 			Published: true,
 			CreatedAt: "today",
 		},
 		{
-			Id: 2,
-			Name: "celu",
-			Colour: "red",
-			Price: 500.5,
-			Stock: 12,
-			Code: "ZZZ",
+			Id:        2,
+			Name:      "celu",
+			Colour:    "red",
+			Price:     500.5,
+			Stock:     12,
+			Code:      "ZZZ",
 			Published: true,
 			CreatedAt: "today",
 		},
@@ -63,15 +69,16 @@ func TestGetAll(t *testing.T) {
 	assert.Equal(t, expected, out)
 }
 
+// UpdateName
 func TestUpdateName(t *testing.T) {
 	data := []Product{
 		{
-			Id: 1,
-			Name: "Before updated",
-			Colour: "black",
-			Price: 200.5,
-			Stock: 12,
-			Code: "XXX",
+			Id:        1,
+			Name:      "Before updated",
+			Colour:    "black",
+			Price:     200.5,
+			Stock:     12,
+			Code:      "XXX",
 			Published: true,
 			CreatedAt: "today",
 		},
@@ -87,4 +94,24 @@ func TestUpdateName(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, expected, out)
 	assert.True(t, db.readWasCalled)
+}
+
+// Store
+func TestStore(t *testing.T) {
+}
+
+// LastID
+func TestLastID(t *testing.T) {
+}
+
+// Update
+func TestUpdate(t *testing.T) {
+}
+
+// UpdateNameAndPrice
+func TestUpdateNameAndPrice(t *testing.T) {
+}
+
+// Delete
+func TestDelete(t *testing.T) {
 }
