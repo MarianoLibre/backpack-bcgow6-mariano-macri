3aad5894bc97f83dacee03f205c93af3ebbbeb27
MarianoLibre <mariano.macri@mercadolibre.com>
2022-11-10 13:02:41 -0300

    initialize mysql db

---
+++ M16/library/cmd/server/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"library.com/db"
+)
+
+func main() {
+
+	fmt.Println("Example")
+	fmt.Println(os.Getenv("SHELL"))
+	fmt.Println("pre: ", os.Getenv("SOMEVAR"))
+
+	if godotenv.Load() != nil {
+		fmt.Println("Can't load env vars");
+		os.Exit(1)
+	}
+
+	fmt.Println("post: ", os.Getenv("SOMEVAR"))
+	fmt.Println("post: [U]", os.Getenv("USERNAME"))
+	fmt.Println("post: [u]", os.Getenv("user"))
+	fmt.Println("post: ", os.Getenv("PASS"))
+	fmt.Println("post: ", os.Getenv("DB"))
+
+	fmt.Println("End")
+	
+	db.Init()
+	db.DataBase.Ping()
+}
---
+++ M16/library/db/db.go
@@ -0,0 +1,38 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var DataBase *sql.DB
+
+func Init() {
+	var err error
+	if err = godotenv.Load(); err != nil {
+		panic(err)
+	}
+	source := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s",
+		os.Getenv("USERNAME"),
+		os.Getenv("PASS"),
+		os.Getenv("SERVER"),
+		os.Getenv("PORT"),
+		os.Getenv("DB"),
+	)
+
+	DataBase, err = sql.Open("mysql", source)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = DataBase.Ping(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("DataBase initialized")
+}
